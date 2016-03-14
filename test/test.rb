eea312e256c61a2f957b3e630bed7c0d32b6b8fb
m <kittelmann@sub.uni-goettingen.de>
2016-03-14 09:37:35 +0100

    Corrections to make test/test.rb work

--- test/test.rb
+++ test/test.rb
@@ -1,6 +1,6 @@
 load '../lib/refine.rb'
 
-prj = Refine.new('date cleanup', 'dates.txt')
+prj = Refine.new({ "project_name" => 'date cleanup', "file_name" => 'dates.csv' })
 prj.apply_operations('operations.json')
 puts prj.export_rows('csv')
 prj.delete_project
